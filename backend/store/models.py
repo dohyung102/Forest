272c11244df714b313023c9ff0a6a15c363f53b2
dohyung102 <87457598+dohyung102@users.noreply.github.com>
2022-03-28 10:47:04 +0900

    REFAC: product restapi modify
    상품관련 rest api 수정(상품 CRUD, 구매내역 등록 및 조회, 장바구니 CRUD, 상품 리뷰 CRUD)

--- backend/store/models.py
+++ backend/store/models.py
@@ -6,9 +6,6 @@ from accounts.models import User
 # Create your models here.
 class Store(models.Model):
     name = models.CharField(default='', max_length=50)
-<<<<<<< HEAD
     profile_image = models.ImageField(upload_to='images/stores/', blank=True, null=True)
-=======
-    profile_image = models.CharField(default='', max_length=100)
     user = models.OneToOneField(User, on_delete=models.CASCADE, default='')
->>>>>>> 2f07ce2509cf6ac89b04e1a42af8bad77bf94bb7
+
