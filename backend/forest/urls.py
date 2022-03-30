11e92855a841a02ce16358b9fa1e6d6a5a4a73c4
김성우 <swkim0128@naver.com>
2022-03-30 14:59:19 +0900

    BLD: add and modify django, docker-compose
    장고, 도커 관련 빌드 수정

--- backend/forest/urls.py
+++ backend/forest/urls.py
@@ -28,25 +28,18 @@ urlpatterns = [
     ### admin
     path('admin/', admin.site.urls),
 
-    path('', include(router.urls)),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/', include(router.urls)),
+    path('api/api-auth', include('rest_framework.urls', namespace='rest_framework')),
 
     ### social login
-    path('users/login/', LoginView.as_view()),
-    path('users/logout/', LogoutView.as_view()),
-    path('users/signup/', RegisterView.as_view(), name='rest_signup'),
+    path('api/users/login/', LoginView.as_view()),
+    path('api/users/logout/', LogoutView.as_view()),
+    path('api/users/signup/', RegisterView.as_view(), name='rest_signup'),
 
-    path('users/verify-email/',
-         VerifyEmailView.as_view(), name='rest_verify_email'),
-    path('users/account-confirm-email/',
-         VerifyEmailView.as_view(), name='account_email_verification_sent'),
-    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$',
-         VerifyEmailView.as_view(), name='account_confirm_email'),
-    path('users/account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
-    path('accounts/', include('dj_rest_auth.urls')),
+    path('api/accounts/', include('dj_rest_auth.urls')),
     # path('accounts/registration/', include('dj_rest_auth.registration.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('accounts/', include('accounts.urls')), 
+    path('api/accounts/', include('allauth.urls')),
+    path('api/accounts/', include('accounts.urls')), 
 
     # # 유효한 이메일이 유저에게 전달
     # re_path(r'^account-confirm-email/$', VerifyEmailView.as_view(), name='account_email_verification_sent'),
@@ -54,8 +47,8 @@ urlpatterns = [
     # re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
 
     ### my apps
-    path('plants/', include('plant.urls')),
-    path('products/', include('product.urls')),
-    path('stores/', include('store.urls')),
-    path('posts/', include('post.urls'))
+    path('api/plants/', include('plant.urls')),
+    path('api/products/', include('product.urls')),
+    path('api/stores/', include('store.urls')),
+    path('api/posts/', include('post.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
