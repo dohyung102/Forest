e3000fbbc21788918d7d47059c653cf70604eea1
김성우 <swkim0128@naver.com>
2022-03-30 11:08:38 +0900

    BLD: add and modify django & docker-compose & dockerfile
     빌드 관련하여 도커파일, 도커컴포즈, django setting 파일 수정 및 추가
     도커 컴포즈에 nginx_proxy 컨테이너 추가 및 도커 파일 추가
     장고 빌드 세팅 관련하여 개발 및 배포 환경 분리

---
+++ backend/forest/settings/prod.py
@@ -0,0 +1,17 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': { 
+    	'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'forest', 
+        'USER': 'ssafy', 
+        'PASSWORD': 'ssafy', 
+        'HOST': 'db', 
+        'PORT': '3306',
+     } 
+}
