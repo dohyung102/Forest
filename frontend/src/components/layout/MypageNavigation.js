fe90301e525e4b386f0a061a6249ef860aa0965e
LEMONRIME <leejsl96@gmail.com>
2022-04-01 17:36:54 +0900

    REFAC: remove code
    불필요 코드 일부 삭제

--- frontend/src/components/layout/MypageNavigation.js
+++ frontend/src/components/layout/MypageNavigation.js
@@ -3,9 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 
 const MypageNavigation = () => {
 
-  // console.log('props',props.data)
   const navigate = useNavigate()
-
   const profileNavigate = (page) => {
     // navigate(`/mypage${page}`, { state: props.userData })
     navigate(`/mypage${page}`)
@@ -14,12 +12,9 @@ const MypageNavigation = () => {
   return (
     <div>
       <ul className='nav-ul'>
-        <li><Link to='/mypage'>프로필</Link></li>
-        <li><Link to='/mypage/posts'>내 글</Link></li>
-        <li><Link to='/mypage/reviews'>내 댓글</Link></li>
         <li onClick={(e) => {profileNavigate('', e)}}>프로필</li>
         <li onClick={(e) => {profileNavigate('/posts', e)}}>내 글</li>
-        <li onClick={(e) => {profileNavigate('/reviews', e)}}>내 댓글</li>
+        <li onClick={(e) => {profileNavigate('/comments', e)}}>내 댓글</li>
       </ul>
     </div>
   );
