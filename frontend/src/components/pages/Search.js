501406fc08f4d3463d8e93735e76f694e2f02564
김성우 <swkim0128@naver.com>
2022-04-04 22:52:19 +0900

    FIX: modify

--- frontend/src/components/pages/Search.js
+++ frontend/src/components/pages/Search.js
@@ -80,7 +80,7 @@ const Search = () => {
             <img
               className="home-plant-img"
               src={`http://j6d204.p.ssafy.io/${plant_data.image_path.substr(
-                6
+                7
               )}`}
               alt="plant_img"
             />
