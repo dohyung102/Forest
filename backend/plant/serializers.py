40e62e9a681805c20cb03e5d540b4348f41fb559
dohyung102 <87457598+dohyung102@users.noreply.github.com>
2022-04-04 09:33:29 +0900

    REFAC : modify_plant_related_product
    식물에서 관련 상품 조회할 수 있도록 수정

--- backend/plant/serializers.py
+++ backend/plant/serializers.py
@@ -2,6 +2,14 @@ from dataclasses import field
 from rest_framework import serializers
 from .models import Plant
 from .recomm_functions import find_similar_plant_by_plant_id
+from product.models import Product
+
+class ProductSerializers(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Product
+        fields = '__all__'
+
 
 class PlantListSerializers(serializers.ModelSerializer):
     
@@ -12,6 +20,7 @@ class PlantListSerializers(serializers.ModelSerializer):
 
 class PlantSerializers(serializers.ModelSerializer):
     similar_plants = serializers.SerializerMethodField()
+    product_set = ProductSerializers(read_only=True, many=True)
 
     class Meta:
         model = Plant
