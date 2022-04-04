4e1646244588ff836c1935264000d5edb841d8b4
김성우 <swkim0128@naver.com>
2022-04-04 14:30:09 +0900

    FIX: modify

--- frontend/src/components/layout/Layout.js
+++ frontend/src/components/layout/Layout.js
@@ -1,30 +1,29 @@
-<<<<<<< HEAD
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Appbar from './Appbar';
-import CartTest from '../pages/CartTest'
-import Carousel from '../pages/Carousel'
-import Survey from '../pages/Survey'
-import SellRegist from '../pages/SellRegist'
-import Home from '../pages/Home'
-import Signup from '../pages/Signup'
-import Login from '../pages/Login'
-import Mypage from '../pages/Mypage'
-import Myposts from '../pages/Myposts'
-import Myreviews from '../pages/Myreviews'
-import Search from '../pages/Search'
-import Community from '../pages/Community'
-import Post from '../pages/Post'
-import Write from '../pages/Write'
-import Detail from '../pages/Detail'
-import Product from '../pages/Product'
-import Store from '../pages/Store'
+import CartTest from '../pages/CartTest';
+import Carousel from '../pages/Carousel';
+import Survey from '../pages/Survey';
+import SellRegist from '../pages/SellRegist';
+import Home from '../pages/Home';
+import Signup from '../pages/Signup';
+import Login from '../pages/Login';
+import Mypage from '../pages/Mypage';
+import Myposts from '../pages/Myposts';
+import Myreviews from '../pages/Myreviews';
+import Search from '../pages/Search';
+import Community from '../pages/Community';
+import Post from '../pages/Post';
+import Write from '../pages/Write';
+import Detail from '../pages/Detail';
+import Product from '../pages/Product';
+import Store from '../pages/Store';
 
 const Layout = () => {
   return (
     <Router>
       <Appbar />
-      <div className='layout'>
+      <div className="layout">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/carttest" element={<CartTest />} />
@@ -48,50 +47,5 @@ const Layout = () => {
     </Router>
   );
 };
-=======
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// import Appbar from './Appbar';
-
-// import Home from '../pages/Home'
-// import Signup from '../pages/Signup'
-// import Login from '../pages/Login'
-// import Mypage from '../pages/Mypage'
-// import Myposts from '../pages/Myposts'
-// import Myreviews from '../pages/Myreviews'
-// import Search from '../pages/Search'
-// import Community from '../pages/Community'
-// import Post from '../pages/Post'
-// import Write from '../pages/Write'
-// import Detail from '../pages/Detail'
-// import Product from '../pages/Product'
-// import Store from '../pages/Store'
-
-// const Layout = () => {
-//   return (
-//     <Router>
-//       <Appbar />
-//       <div className='layout'>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/signup" element={<Signup />} />
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/mypage" element={<Mypage />} />
-//           <Route path="/mypage/posts" element={<Myposts />} />
-//           <Route path="/mypage/reviews" element={<Myreviews />} />
-//           <Route path="/search" element={<Search />} />
-//           <Route path="/detail/:plant" element={<Detail />} />
-//           <Route path="/product/:plant" element={<Product />} />
-//           <Route path="/community" element={<Community />} />
-//           <Route path="/community/:post_pk" element={<Post />} />
-//           <Route path="/community/write" element={<Write />} />
-//           <Route path="/store" element={<Store />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// };
->>>>>>> a721da454ea99ec76acd8ec5f1493be585e344b8
 
 // export default Layout;
