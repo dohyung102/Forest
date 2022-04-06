80899724c737da7d0d94d2536b43fd872776e89a
김성우 <swkim0128@naver.com>
2022-04-06 18:06:39 +0900

    FIX: modify

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,6 +1,7 @@
 from distutils.command.upload import upload
 from email.policy import default
 from django.db import models
+from numpy import blackman
 from accounts.models import User
 
 # Create your models here.
@@ -8,7 +9,7 @@ from accounts.models import User
 class Post(models.Model):
     title = models.CharField(default='', max_length=50)
     content = models.CharField(default='', max_length=50)
-    image = models.ImageField(upload_to='images/posts/', null=True, default='', max_length=50)
+    image = models.ImageField(upload_to='images/posts/', null=True, blank=True, default='', max_length=50)
     tag = models.CharField(default='', null=True, max_length=50)
     created_date = models.DateField(auto_now_add=True)
 
